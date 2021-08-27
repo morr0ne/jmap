46c3bd6376bc26887b796a323300e1b720931bd7
Federico Morrone <contact@morrone.dev>
2021-08-27 03:01:55 +0200

    Added echo command

--- examples/simple.rs
+++ examples/simple.rs
@@ -16,10 +16,11 @@ async fn main() -> anyhow::Result<()> {
     let email = matches.value_of("email").unwrap();
     let password = matches.value_of("password").unwrap();
 
-    let client = ClientBuilder::new().build()?;
+    let mut client = ClientBuilder::new().build()?;
     let session = client.auth(server, email, password).await?;
 
-    let res = client.send(&session).await?;
+    let res = client.echo(&session).await?;
+    dbg!(res);
 
     fs::write("temp/jmap.json", serde_json::to_vec(&session)?).await?;
 

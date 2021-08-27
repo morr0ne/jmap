4cb307206ccb0a825e9a3b6dc054992b5a75d20c
Federico Morrone <contact@morrone.dev>
2021-08-27 16:02:23 +0200

    Added mailbox object

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,9 +1,11 @@
 pub mod common;
+pub mod mailbox;
 pub mod request;
 pub mod response;
 pub mod session;
 
 pub use common::*;
+pub use mailbox::*;
 pub use request::*;
 pub use response::*;
 pub use session::*;

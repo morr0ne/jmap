ef4f7333e709417785ba133e2b9b1a06c34df0b9
Federico Morrone <contact@morrone.dev>
2021-08-31 01:12:11 +0200

    Fixed some types + added request builder

--- src/types/invocation.rs
+++ src/types/invocation.rs
@@ -3,4 +3,4 @@ use serde::{Deserialize, Serialize};
 use super::JsonValue;
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct Invocation<T = JsonValue>(pub String, pub indexmap::IndexMap<String, T>, pub String);
+pub struct Invocation<T = indexmap::IndexMap<String, JsonValue>>(pub String, pub T, pub String);

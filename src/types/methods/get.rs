7ccdd2a3c9abdd5f5e9f88ff5ba6aa33507eef64
Federico Morrone <contact@morrone.dev>
2021-08-30 03:33:51 +0200

    Added get request

--- src/types/methods/get.rs
+++ src/types/methods/get.rs
@@ -1,5 +1,15 @@
 use serde::{Deserialize, Serialize};
 
+use crate::types::Id;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Request {
+    #[serde(rename = "accountId")]
+    pub account_id: Id,
+    pub ids: Option<Vec<Id>>,
+    pub properties: Option<Vec<String>>,
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Get<T> {
     pub state: String,

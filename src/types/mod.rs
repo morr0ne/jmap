90b4a45a87edfb9ad339718b4c0278d20ecf431e
Federico Morrone <contact@morrone.dev>
2021-08-30 03:12:52 +0200

    Moved mailbox to mail types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,11 +1,11 @@
 pub mod common;
-pub mod mailbox;
+pub mod mail;
 pub mod request;
 pub mod response;
 pub mod session;
 
 pub use common::*;
-pub use mailbox::*;
+pub use mail::*;
 pub use request::*;
 pub use response::*;
 pub use session::*;

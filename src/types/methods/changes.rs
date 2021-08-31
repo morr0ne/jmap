e8e02b6aef08e1c9545989b996f8e5e24950fe10
Federico Morrone <contact@morrone.dev>
2021-08-31 18:27:02 +0200

    Fixed all clippy warnings

--- src/types/methods/changes.rs
+++ src/types/methods/changes.rs
@@ -6,16 +6,20 @@ use crate::types::Id;
 pub struct Request {
     #[serde(rename = "accountId")]
     pub account_id: Id,
-    pub maxChanges: Option<u32>,
+    #[serde(rename = "maxChanges")]
+    pub max_changes: Option<u32>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Response {
     #[serde(rename = "accountId")]
     pub account_id: Id,
-    pub oldState: String,
-    pub newState: String,
-    pub hasMoreChanges: bool,
+    #[serde(rename = "oldState")]
+    pub old_state: String,
+    #[serde(rename = "newState")]
+    pub new_state: String,
+    #[serde(rename = "hasMoreChanges")]
+    pub has_more_changes: bool,
     pub created: Vec<Id>,
     pub updated: Vec<Id>,
     pub destroyed: Vec<Id>,
